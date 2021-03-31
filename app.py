9a312fed39e880337c52f560b5b2fff744e4cb0c
rgoshen <rick.goshen@gmail.com>
2021-03-31 15:23:43 -0700

    setup basic flask app

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
-"""Blogly application."""
+"""Feedback application."""
 
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+# from models import db, connect_db
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_ECHO'] = True
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///feedback_db'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = "ihopethephilliescanwin"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
-connect_db(app)
-db.create_all()
+# connect_db(app)
+# db.drop_all()
+# db.create_all()
+
+toolbar = DebugToolbarExtension(app)
+
+
+@app.route('/')
+def home_page():
+    """Homepage of site."""
+    return render_template('index.html')
