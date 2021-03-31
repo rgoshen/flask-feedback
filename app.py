6c220d0f2e5bc7e0fc889d7d1216bc4f71a07d43
rgoshen <rick.goshen@gmail.com>
2021-03-31 16:19:33 -0700

    added user model

--- app.py
+++ app.py
@@ -2,18 +2,18 @@
 
 from flask import Flask, render_template
 from flask_debugtoolbar import DebugToolbarExtension
-# from models import db, connect_db
+from models import db, connect_db, User
 
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///feedback_db'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# app.config['SQLALCHEMY_ECHO'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///feedback_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
 app.config['SECRET_KEY'] = "ihopethephilliescanwin"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
-# connect_db(app)
-# db.drop_all()
-# db.create_all()
+connect_db(app)
+db.drop_all()
+db.create_all()
 
 toolbar = DebugToolbarExtension(app)
 
